5295a87a52f6fff2cb3add0b82af238b2640297d
mephi007 <roysumit949@gmail.com>
2020-06-21 05:33:37 +0530

    using semaphore

--- Synchronization/src/mechanisms/ProducerConsumerUsingMutex.java
+++ Synchronization/src/mechanisms/ProducerConsumerUsingMutex.java
@@ -6,7 +6,8 @@ import java.util.Random;
 import java.util.concurrent.Semaphore;
 
 public class ProducerConsumerUsingMutex {
-	public static Semaphore mutex = new Semaphore(1);
+	public static Semaphore con = new Semaphore(0);
+	public static Semaphore pro = new Semaphore(1);
 	public static Queue<Integer> q = new LinkedList();
 	public static Random random = new Random();
 	public static int cap = 10;
@@ -25,19 +26,24 @@ public class ProducerConsumerUsingMutex {
 			int num;
 			while (true) {
 				try {
-					mutex.acquire();
-					try {
-						num = random.nextInt(100);
-						q.add(num);
-						System.out.println("produced " + num);
-						System.out.println("Queue size " + q.size());
-					}finally {
-						mutex.release();
-					}
-					
+					pro.acquire();
 				}catch(Exception e) {
 					
 				}
+				num = random.nextInt(100);
+				if (q.size() == cap) {
+					try {
+						wait();
+					} catch (InterruptedException e) {
+
+					}
+				}
+				q.add(num);
+				System.out.println("produced " + num);
+				System.out.println("Queue size " + q.size());
+				
+					con.release();
+				
 				
 			}
 		}
@@ -49,19 +55,23 @@ public class ProducerConsumerUsingMutex {
 			int num;
 			while (true) {
 				try {
-					mutex.acquire();
-					try {
-						
-						num = q.poll();
-						System.out.println("produced " + num);
-						System.out.println("Queue size " + q.size());
-					}finally {
-						mutex.release();
-					}
-					
+					con.acquire();
 				}catch(Exception e) {
 					
 				}
+				if (q.size() == 0) {
+					try {
+						wait();
+					} catch (InterruptedException e) {
+
+					}
+				}
+				num = q.poll();
+				System.out.println("consumed " + num);
+				System.out.println("Queue size " + q.size());
+				
+					pro.release();
+				
 				
 			}
 		}
